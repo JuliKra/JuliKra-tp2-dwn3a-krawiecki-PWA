df53750adce2493cc572b28ef128f62dddd96264
JuliKra <69880741+JuliKra@users.noreply.github.com>
2021-07-01 12:19:27 -0300

    Add files via upload

---
+++ js/register-sw.js
@@ -0,0 +1,19 @@
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('../service-worker.js')
+      .then(reg => {
+        console.log("Service worker esta listo!");
+      });
+}
+else {
+  console.log("Service worker no soportado.");
+}
+
+window.addEventListener('offline', event => {
+  document.querySelector('body').classList.add('offline');
+  main.innerHTML = "La aplicacion esta offline!"
+});
+
+window.addEventListener('online', event => {
+  document.querySelector('body').classList.remove('offline');
+  openSoccerApi();
+});
